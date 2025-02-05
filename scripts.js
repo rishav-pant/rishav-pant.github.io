ce7d2bbbc4c67e91ce74c3f0478f1b0e7d2eb066
rishav-pant <patiencepant@gmail.com>
2025-02-05 00:19:14 +0000

    5th Feb: Fixed some out of order skill trails issues

---
+++ scripts.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    setInterval(createJugglingSkill, 600); // Call this immediately once the page has loaded
+};
+const skills = ['Bash', 'C', 'Go', 'Java', 'JavaScript', 'Programming Languages', 'Python', 'Redux', 'TypeScript', 'Customer Satisfaction', 'Email', 'Forensics', 'Leadership', 'Management', 'Mentoring', 'Onboarding', 'Presence', 'Problem Solving', 'Reporting', 'Workday', 'Amazon EC2', 'Amazon Web Services', 'Analytical', 'Ansible', 'Apps', 'Automation', 'Bash Shell', 'Cloud', 'Cyber Security', 'Debugging', 'Deployment', 'DevOps', 'Django', 'Docker', 'Firebase', 'Git', 'GitHub', 'Google Cloud', 'Google Cloud Platform', 'Hibernate', 'Internet', 'JScript', 'Kubernetes', 'Linux', 'Monitoring', 'MySQL', 'Networking', 'Node.js', 'OpenCV', 'OpenStack', 'Packet Tracer', 'Performance', 'Research', 'Shell Scripting', 'Site Reliability Engineering', 'Social', 'Software Development', 'Troubleshooting', 'Unix', 'Python', 'AWS EKS', 'AWS ECS', 'Amazon Web Services'];
+
+// Creating a container for skill lines inside the hero section
+const skillContainer = document.createElement('div');
+skillContainer.classList.add('skill-container');
+document.querySelector('.hero').appendChild(skillContainer);
+let line = 0;
+
+function createJugglingSkill() {
+    const skill = document.createElement('div');
+    skill.classList.add('skill-word-'+line);
+    skill.textContent = skills[Math.floor(Math.random() * skills.length)];
+    
+    // Set the vertical position of the skill word
+    skill.style.top = `${line * 17}%`;
+    
+    // Append the skill to the container
+    skillContainer.appendChild(skill);
+    
+    // Remove the word after the animation duration to clean up the DOM
+    setTimeout(() => {
+        skill.remove();
+    }, 10000);  // Matches animation duration (10s)
+    line += 1;
+    line %= 6;
+}
